4186b24a6dc245a5e8ec1cdffd4e16536bd1ed01
SendHelp001 <jonardsalanip@gmail.com>
2024-12-16 22:46:57 +0800

    put comments on routes to increase readability

--- routes/web.php
+++ routes/web.php
@@ -14,10 +14,6 @@ Route::get('/', function () {
     return view('welcome');
 });
 
-Route::get('/dashboard', function () {
-    return view('dashboard');
-})->middleware(['auth', 'verified'])->name('dashboard');
-
 
 
 Route::middleware('auth')->group(function () {
@@ -30,32 +26,41 @@ Route::middleware('auth')->group(function () {
 
 
 
-require __DIR__.'/auth.php';
-
-Route::get('/dashboard', function () {
-    return view('dashboard');
-})->middleware(['auth', 'verified'])->name('dashboard');
+require __DIR__ . '/auth.php';
 
 
+
+//EMPLOYEE ROUTES
 Route::get('/employees', [employeeController::class, 'index'])->name('employees.index');
 Route::get('/employees/create', [employeeController::class, 'create'])->name('employees.create');
 Route::post('/employees', [employeeController::class, 'store'])->name('employees.store');
 Route::get('/employees/{id}/edit', [employeeController::class, 'edit'])->name('employees.edit');
 Route::put('/employees/{id}', [employeeController::class, 'update'])->name('employees.update');
 Route::delete('/employees/{id}', [employeeController::class, 'destroy'])->name('employees.destroy');
-
-
 Route::get('/employee', [employee_user_viewController::class, 'show'])->middleware('auth')->name('employee');
 
+//PROFILE ROUTES
 Route::get('/profile', [employee_user_viewController::class, 'index'])->name('profile-information')->middleware('auth');
 
+//ROUTES TO BE CLASSIFIED
 Route::get('/department', [departmentController::class, 'index'])->name('department')->middleware('auth');
 Route::get('/attendance', [attendanceController::class, 'index'])->name('attendance')->middleware('auth');
 Route::get('/leaves', [leavesController::class, 'index'])->name('leaves')->middleware('auth');
 Route::get('/payroll', [payrollController::class, 'index'])->name('payroll')->middleware('auth');
 Route::get('/performance', [performanceController::class, 'index'])->name('performance')->middleware('auth');
 
+
+//DASHBOARD ROUTES
+
+Route::get('/dashboard', function () {
+    return view('dashboard');
+})->middleware(['auth', 'verified'])->name('dashboard');
+Route::get('/dashboard', function () {
+    return view('dashboard');
+})->middleware(['auth', 'verified'])->name('dashboard');
 Route::get('/manager/dashboard', function () {
-    return view('dashboard');});
+    return view('dashboard');
+});
 Route::get('/admin/dashboard', function () {
-    return view('dashboard');});
+    return view('dashboard');
+});
