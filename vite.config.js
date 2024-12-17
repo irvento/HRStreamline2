36a2e787a5f21ee3664de9133d1e93139bb195df
SendHelp001 <jonardsalanip@gmail.com>
2024-12-17 18:56:16 +0800

    INSERT HOST FIX

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,7 @@ import laravel from 'laravel-vite-plugin';
 
 export default defineConfig({
     server: {
-       host: "192.168.1.2",
+     //  host: "192.168.1.2" INSERT HOST
     },
     plugins: [
         laravel({
