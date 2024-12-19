025ca69fb7700bb2dc0d6eebce5f71321ece1bc7
irvn <129978135+irvento@users.noreply.github.com>
2024-12-19 22:43:10 +0800

    added image routing

--- routes/web.php
+++ routes/web.php
@@ -134,7 +134,7 @@ Route::middleware('auth')->group(function () {
     Route::get('/reports/performance', [employee_info_viewController::class, 'performance'])->name('rperformance');
     Route::get('/reports/salary-reports', [employee_info_viewController::class, 'salaryReports'])->name('rsalary.reports');
     Route::get('/reports/department-analysis', [employee_info_viewController::class, 'departmentAnalysis'])->name('rdepartment.analysis');
-    Route::get('/reports/custom-reports', [employee_info_viewController::class, 'customReports'])->name('rcustom.reports');
+    
     // QUALIFICATIONS ROUTES
     Route::get('/qualifications', [qualificationsController::class, 'index'])->name('qualifications.index');
 
